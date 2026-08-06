38308925f29e95acc911092e91803de35ed68639
misumisumi <s.kobayashi@misumi-sumi.com>
2026-08-06 19:28:29 +0900

    fix: external-dns pdns server URL missing slashes

--- modules/external-dns.nix
+++ modules/external-dns.nix
@@ -38,7 +38,7 @@
         txtOwnerId = "home-k8s-cluster";
         policy = "sync";
         extraArgs = [
-          "--pdns-server=http:172.16.1.2:8081"
+          "--pdns-server=http://172.16.1.2:8081"
           "--pdns-api-key=hogehoge"
         ];
       };
